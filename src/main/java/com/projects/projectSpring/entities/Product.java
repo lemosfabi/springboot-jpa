3180bd6b44c86df537d606d717cda9ad9414ebf0
Fabi Lemos <fabianalemos@gmail.com>
2025-02-26 14:36:23 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/projects/projectSpring/entities/Product.java
+++ src/main/java/com/projects/projectSpring/entities/Product.java
@@ -7,12 +7,15 @@ import java.util.Set;
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.JoinColumn;
 
@@ -35,7 +38,10 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category",  joinColumns = @JoinColumn(name = "product_id"), 
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-		
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 		
 	}
@@ -107,12 +113,22 @@ public class Product implements Serializable {
 	}
 
 
+	@JsonIgnore   
+    public Set<Order> getOrders(){
+    	Set<Order> set = new HashSet<>();
+    	for (OrderItem x : items) {
+    		set.add(x.getOrder());
+    	}
+    	return set;
+    }
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
+	
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
